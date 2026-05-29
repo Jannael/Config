6ffd2eae3df639b1b78f5a857f6fcdab5876d5dd
Jannael <orlandojannael@gmail.com>
2026-05-28 18:13:10 -0600

    feat: map for supported tech

---
+++ apps/cli/data/index.ts
@@ -0,0 +1,17 @@
+const technologies: Record<string, string> = {
+  angular: "Angular",
+  astro: "Astro",
+  lit: "Lit",
+  next: "Next.js",
+  nuxt: "Nuxt",
+  qwik: "Qwik",
+  react: "React",
+  "react-native": "React Native",
+  remix: "Remix",
+  solid: "Solid",
+  svelte: "Svelte",
+  sveltekit: "SvelteKit",
+  vue: "Vue",
+}
+
+export default technologies
