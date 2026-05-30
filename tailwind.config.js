575a7bc090736693541e837764e44e3de3d7f248
Jannael <orlandojannael@gmail.com>
2026-05-30 13:16:34 -0600

    chore: format

--- tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: ['./apps/web/src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
