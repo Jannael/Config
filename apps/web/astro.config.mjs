0178634d5ecfed1cfa074697662c21cfdb7a4c55
Jannael <orlandojannael@gmail.com>
2026-05-30 15:12:06 -0600

    fix: add site in astro config for sitemap to work

--- apps/web/astro.config.mjs
+++ apps/web/astro.config.mjs
@@ -5,6 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://config.jannael.com',
   vite: {
     plugins: [tailwindcss()],
   },
