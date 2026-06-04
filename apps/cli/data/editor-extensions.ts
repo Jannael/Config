f8ff12e1fc866603a57f56b8a5eb943f0f798bc0
Jannael <orlandojannael@gmail.com>
2026-06-04 10:05:47 -0600

    feat: more config stuff

---
+++ apps/cli/data/editor-extensions.ts
@@ -0,0 +1,22 @@
+export const extensionLinks = {
+  biome: {
+    name: 'Biome',
+    vsc: 'https://marketplace.visualstudio.com/items?itemName=biomejs.biome',
+    vsx: 'https://open-vsx.org/extension/biomejs/biome',
+  },
+  oxlint: {
+    name: 'Oxc',
+    vsc: 'https://marketplace.visualstudio.com/items?itemName=oxc.oxc-vscode',
+    vsx: 'https://open-vsx.org/extension/oxc/oxc-vscode',
+  },
+  eslint: {
+    name: 'ESLint',
+    vsc: 'https://marketplace.visualstudio.com/items?itemName=dbaeumer.vscode-eslint',
+    vsx: 'https://open-vsx.org/extension/dbaeumer/vscode-eslint',
+  },
+  prettier: {
+    name: 'Prettier',
+    vsc: 'https://marketplace.visualstudio.com/items?itemName=esbenp.prettier-vscode',
+    vsx: 'https://open-vsx.org/extension/esbenp/prettier-vscode',
+  },
+} as const
