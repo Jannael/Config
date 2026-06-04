9a8b1d49bc5754db1c19e20fae5b97d081fe5db5
Jannael <orlandojannael@gmail.com>
2026-06-04 10:53:03 -0600

    chore: update data types

--- apps/cli/configs/types.d.ts
+++ apps/cli/configs/types.d.ts
@@ -1,25 +1,24 @@
-export interface LinterConfig  {
+export interface LinterConfig {
   label: string
   linter: {
     eslint: {
       plugins: string[]
       config: {
-        path: string,
-        importStatement: string,
-        configSpread: string[],
-        ignorePatterns: string[],
-        fileExtensions: string[],
+        importStatements: string[]
+        configSpread: string[]
+        ignorePatterns: string[]
+        fileExtensions: string[]
       }
     }
-  },
+  }
 }
 
 export interface FormatterConfig {
   formatter: {
     prettier: {
       config: {
-        path: string,
-        importStatement: string,
+        path: string
+        importStatement: string
         configSpread: {
           plugins: string[]
         }
