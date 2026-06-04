ec680ec30ea48b72ac14da908cebf348058df34f
Jannael <orlandojannael@gmail.com>
2026-06-04 10:53:29 -0600

    feat: config stuff

--- apps/cli/infra/generators/biome.ts
+++ apps/cli/infra/generators/biome.ts
@@ -2,11 +2,12 @@ import { writeFileSync } from 'node:fs'
 import { join } from 'node:path'
 
 export function generateBiome({ cwd }: { cwd: string }): void {
+  //this is going to be the baseConfig for biome
   const config = {
     $schema: 'https://biomejs.dev/schemas/2.0.0/schema.json',
     formatter: {
       enabled: true,
-      lineWidth: 100,
+      lineWidth: 150,
       indentStyle: 'space',
       indentWidth: 2,
     },
