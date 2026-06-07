6c78c2e15c56ed3080a8e28c2c43c49dcd2f7e09
Jannael <orlandojannael@gmail.com>
2026-06-07 11:10:29 -0600

    feat: init infra

---
+++ apps/cli/infra/infra.ts
@@ -0,0 +1,53 @@
+import type { Formatters, Linters } from '@/configs/types'
+import { type Repository as IRepository } from '@/domain/repository'
+
+export class Repository implements IRepository {
+  constructor() {}
+
+  async installDependencies({ dependencies }: { dependencies: string[] }): Promise<void> {
+    console.log(dependencies)
+  }
+
+  async writeBiomeConfig(techs: string[]): Promise<void> {
+    console.log(techs)
+  }
+  async writePrettierConfig(techs: string[]): Promise<void> {
+    console.log(techs)
+  }
+  async writeEslintConfig(techs: string[]): Promise<void> {
+    console.log(techs)
+  }
+  async writeOxLintConfig(techs: string[]): Promise<void> {
+    console.log(techs)
+  }
+  async writeOxFmtConfig(techs: string[]): Promise<void> {
+    console.log(techs)
+  }
+
+  async writeLintStagedConfig(
+    formatter: Formatters,
+    linter: Linters,
+    techs: string[],
+  ): Promise<void> {
+    console.log(formatter, linter, techs)
+  }
+  async writeEditorConfig(techs: string[]): Promise<void> {
+    console.log(techs)
+  }
+
+  async writePackageJsonScripts(formatter: Formatters, linter: Linters): Promise<void> {
+    console.log(formatter, linter)
+  }
+  async getEditorExtensions(
+    formatter: Formatters,
+    linter: Linters,
+  ): Promise<{ name: string; vsc: string; vsx: string }[]> {
+    console.log(formatter, linter)
+    return []
+  }
+
+  getAutoApproveFlag(): boolean {
+    return false
+  }
+  async initHusky(): Promise<void> {}
+}
