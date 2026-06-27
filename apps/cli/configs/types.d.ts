63b862f9e8f3b2cd8fd453d06b780d6e99ef9dc3
Jannael <orlandojannael@gmail.com>
2026-06-26 18:36:43 -0600

    chore: commit command for opencode

--- apps/cli/configs/types.d.ts
+++ apps/cli/configs/types.d.ts
@@ -5,7 +5,7 @@ export interface LinterConfig {
 			config?: {
 				importStatements: string[]
 				configSpread: string[]
-				configObject: string[]
+				configObject?: string[]
 				ignorePatterns: string[]
 				fileExtensions: string[]
 			}
